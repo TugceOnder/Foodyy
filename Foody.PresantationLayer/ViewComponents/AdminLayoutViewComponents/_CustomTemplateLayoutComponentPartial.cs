aec153e3f342b336c19f6d9b8649e649b3a1828f
TugceOnder <tugceonder775@gmail.com>
2024-07-22 23:49:05 +0300

    categoryList kısmına yeni özellikler eklendi

---
+++ Foody.PresantationLayer/ViewComponents/AdminLayoutViewComponents/_CustomTemplateLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresantationLayer.ViewComponents.AdminLayoutViewComponents
+{
+	public class _CustomTemplateLayoutComponentPartial: ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
